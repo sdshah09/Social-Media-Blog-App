b12f4a0cbf2ae46f699d1cb1b66e27ac6f492b48
Shaswat Shah <shaswatshah51@gmail.com>
2024-07-30 14:29:52 -0500

    Created a new setup for apollo client in App.js where it can switch between http query and web socket query

--- gql/client/src/App.js
+++ gql/client/src/App.js
@@ -4,43 +4,61 @@ import {
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
+  split,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import { Routes, Route } from "react-router-dom";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from 'graphql-ws';
+import { getMainDefinition } from "@apollo/client/utilities";
+
 import Home from "./pages/Home";
 import Users from "./pages/Users";
 import Nav from "./components/Nav";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import CompleteRegistration from "./pages/auth/CompleteRegistration";
+import SingleUser from "./pages/SingleUser";
+import PasswordUpdate from "./pages/auth/PasswordUpdate";
+import PasswordForgot from "./pages/auth/PasswordForgot";
+import SearchResult from "./components/SearchResults";
+import Profile from "./pages/auth/Profile";
+import Post from "./pages/post/Post";
+import PostUpdate from "./pages/post/PostUpdate";
+import SinglePost from "./pages/post/SinglePost";
 
 import { ToastContainer } from "react-toastify";
 import { AuthContext } from "./context/authContext";
 import PrivateRoute from "./components/PrivateRoute";
 import PublicRoute from "./components/PublicRoute";
-import SingleUser from "./pages/SingleUser";
-import PasswordUpdate from "./pages/auth/PasswordUpdate";
-import PasswordForgot from "./pages/auth/PasswordForgot";
-import SearchResult from "./components/SearchResults";
-
-import Profile from "./pages/auth/Profile";
-import Post from "./pages/post/Post";
-import PostUpdate from "./pages/post/PostUpdate";
-import SinglePost from "./pages/post/SinglePost"
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   const { state } = useContext(AuthContext);
   const { user } = state;
-  useEffect(() => {
-    console.log("User state changed:", state.user);
-  }, [state.user]);
 
+  // 1. Create WebSocket link
+  const wsLink = new GraphQLWsLink(
+    createClient({
+      url: process.env.REACT_APP_WS_ENDPOINT,
+      options: {
+        reconnect: true,
+      },
+    })
+  );
+
+  // 2. Create HTTP link
   const httpLink = createHttpLink({
     uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
   });
 
+  useEffect(() => {
+    console.log("User state changed:", state.user);
+  }, [state.user]);
+
+  // 3. Set context for authentication
   const authLink = setContext((_, { headers }) => {
     const token = user ? user.token : "";
     return {
@@ -51,8 +69,25 @@ const App = () => {
     };
   });
 
+  // 4. Concat HTTP and auth token link
+  const httpAuthLink = authLink.concat(httpLink);
+
+  // 5. Use split to split HTTP link or WebSocket link
+  const link = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === "OperationDefinition" &&
+        definition.operation === "subscription"
+      );
+    },
+    wsLink,
+    httpAuthLink
+  );
+
+  // 6. Create new Apollo Client
   const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link,
     cache: new InMemoryCache(),
   });
 
@@ -111,7 +146,6 @@ const App = () => {
             </PrivateRoute>
           }
         />
-
         <Route
           path="/profile"
           element={
